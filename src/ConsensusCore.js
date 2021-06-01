2668e7239e1cfc16f421fac81ad9057a0add1d89
Alboaie Sînică <salboaie@gmail.com>
2021-06-01 13:05:18 +0300

    Update ConsensusCore.js

--- src/ConsensusCore.js
+++ src/ConsensusCore.js
@@ -1,6 +1,6 @@
 /*
 A configurable consensus core that can have 3 consensus strategies
- - SVBC - Single Validator BrickLedger Consensus: imeditaly accepts a BrickBlock and treat it as a real Block 
+ - SVBC - Single Validator BrickLedger Consensus:  Only one node is accepting commands and propose BrickBlocks. A block has only one BrickBlock.
  - MVBC - Multiple Validators BrickLedger Consensus: run the BrickLedger consensus between validators
  - OBAC - Other Blockchain Adapter Consensus: Delegates Consensus to a blockchain adapter that is using other blockchain network for consensus regrading the blocks of commands 
 */
