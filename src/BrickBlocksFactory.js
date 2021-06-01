6755b5fa6d54eb01c979c1aaaa6bd72e458b0e78
Alboaie Sînică <salboaie@gmail.com>
2021-06-01 12:03:12 +0300

    Update BrickBlocksFactory.js

--- src/BrickBlocksFactory.js
+++ src/BrickBlocksFactory.js
@@ -2,3 +2,31 @@
   Created BrickBlocks and send it to the optimistic execution 
   Save the bricks in the BrickStorage
 */
+
+function createBrickBlock(commandsArray,hashOfThePreviousBlock,blockNumber,validatorDID){
+ 
+  let res = {
+   commandsArray,
+    hashOfThePreviousBlock,
+    blockNumber
+  }
+  res.validatorSIgnature = validatorDID.sign(hash(res));
+  return res;
+}
+
+function createBlock(brickBlocksSet, votingProof, blockNumber, previousBlockHash ){
+  let brickBlocksArray = sort(brickBlocksSet);
+  let res = {
+   brickBlocksArray,
+    votingProof,
+    blockNumber,
+    previousBlockHash
+  }
+  return res;
+}
+
+
+module.exports = {
+ createBrickBlock,
+ createBlock
+}
