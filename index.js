f38e9d82b6ef252bcbd3763b8427017b6b062397
Alboaie Sînică <salboaie@gmail.com>
2021-06-01 17:24:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,38 @@
-function BricksLedger(booter, bbFactory,  broadcaster, consensusCore, executionEngine, bricksStorage, keyValueStorage){
+function BricksLedger(myDID, booter, bbFactory,  broadcaster, consensusCore, executionEngine, bricksStorage, keyValueStorage){
   
-  this.newCommand = function(){
-    
+  this.executeSafeCommand = function(command, callback){
+    let executionResult = await executionEngine.executeMethodOptimistcally(command);
+    if(execution.requireConsensus()){
+      this.executeNoncedCommand(command, callback);
+    }
+    callback(undefined, executionResult)
   }
   
-  
-  this.newBlock = function(){
+  this.executeNoncedCommand = function(command, callback){
+    if(booter.contracts.bdns.isValidator(myDID)){
+      bbFactory.addNoncedCommand(command);
+    } else {
+      let validator = async booter.contracts.bdns.chooseValidator();
+      broadcaster.forwardCommand(validator, command, callback);  //pass the command to an real validator
+    }      
+  }
     
+  this.newBrickBlockFromNetwork = function(brickBlock){
+    consensusCore.addInConsensus(brickBlock);
   }
   
 }
 
-module.exports.initiliseBrickLedger = function(domain){
-  let bricksStorage   = require("./src/FSBricksStorage.js").create(domain); 
-  let keyValueStorage = require("./src/FSKeyValueStorage.js").create(domain); 
+module.exports.initiliseBrickLedger = function(domain, config, notificationHandler){
+  let booter          = require("./src/booter.js").create(domain, config); 
+  
+  let bricksStorage   = require("./src/FSBricksStorage.js").create(domain,config); 
+  let keyValueStorage = require("./src/FSKeyValueStorage.js").create(domain, config); 
   let bbFactory       = require("./src/BrickBlocksFactory.js").create(domain);
-  let executionEngine = require("./src/ExecutionEngine.js").create(domain);
+  let executionEngine = require("./src/ExecutionEngine.js").create(domain, notificationHandler);
   let consensusCore   = require("./src/ConsensusCore.js").create(domain);
   let broadcaster     = require("./src/broadcaster.js").create(domain); 
-  
-  let booter          = require("./src/booter.js").create(domain); 
+   
   
   return new BricksLedger(bbFactory,  broadcaster, consensusCore, executionEngine, bricksStorage, keyValueStorage);
 }
