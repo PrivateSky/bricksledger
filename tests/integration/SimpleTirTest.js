512fbc74bb02e86e93c07fbb3f458f0a73e9ca4d
skutner <sava.dumitru.daniel@gmail.com>
2021-10-15 12:08:17 +0300

    refactored tir

--- tests/integration/SimpleTirTest.js
+++ tests/integration/SimpleTirTest.js
@@ -1,22 +1,21 @@
 require("../../../../psknode/bundles/testsRuntime");
-const { launchApiHubTestNode } = require("../../../../psknode/tests/util/tir");
+const {launchApiHubTestNode} = require("../../../../psknode/tests/util/tir");
 
 const dc = require("double-check");
 const assert = dc.assert;
 
 const bricksledger = require("../../index");
+const tir = require("../../../../psknode/tests/util/tir");
 
 assert.callback(
     "Simple TIR test",
     async (testFinished) => {
-        try {
-            const result = await $$.promisify(launchApiHubTestNode)();
-            assert.notNull(result);
+        dc.createTestFolder('AddFilesBatch', async (err, folder) => {
+                const result = await $$.promisify(launchApiHubTestNode)(100, folder);
+                assert.notNull(result);
 
-            testFinished();
-        } catch (error) {
-            console.error(error);
-        }
+                testFinished();
+            });
     },
     20000
 );
